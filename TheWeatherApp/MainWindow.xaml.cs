fd3e2ccfd05981f2b7aac594db80078e757596a0
arthur <arthurfoadjo@yahoo.com>
2022-06-24 17:56:53 +0200

    date display

--- TheWeatherApp/MainWindow.xaml.cs
+++ TheWeatherApp/MainWindow.xaml.cs
@@ -20,19 +20,15 @@ namespace TheWeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public WeatherData weather = new WeatherData();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            // Changes the image by getting the 
-            // Create source
-            BitmapImage bi = new BitmapImage();
-            bi.BeginInit();
-            bi.UriSource = new Uri(@"/rain.jpg", UriKind.Relative);
-            bi.EndInit();
-            //Set the image source
-            headerImage.Source = bi;
-            //headerImage.Source = new BitmapImage(new Uri(@"/rain.jpg", UriKind.Relative));
+            SetHeaderImage();
+            SetDay(); //Date
+         
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
@@ -43,6 +39,52 @@ namespace TheWeatherApp
         private void buttonInfo_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Set the Date of the current day
+        /// </summary>
+        public void SetDay()
+        {
+            //To-Do: Implement test for the function
+            DateTime dateTime = DateTime.Now;
+            weather.Date = dateTime;
+            dateText.Content = weather.Date.ToString("MM/dd/yyyy");
+
+            // Date of the first day
+            weather.Forecast = weather.Date.ToString("ddd");
+            forecast1.Content = weather.Forecast;
+
+            // Date of the second day
+            weather.Forecast = weather.Date.AddDays(1).ToString("ddd");
+            forecast2.Content = weather.Forecast;
+
+            // Date of the third day
+            weather.Forecast = weather.Date.AddDays(2).ToString("ddd");
+            forecast3.Content = weather.Forecast;
+        }
+
+        public void SetHeaderImage()
+        {
+            string meteo = "pluie";
+
+            // Changes the image by getting the 
+            // Create source
+            BitmapImage bi = new BitmapImage();
+            //Set the image source
+            headerImage.Source = bi;
+            //headerImage.Source = new BitmapImage(new Uri(@"/rain.jpg", UriKind.Relative));
+
+            if (meteo == "pluie")
+            {
+                bi.BeginInit();
+                bi.UriSource = new Uri(@"/rain.jpg", UriKind.Relative);
+                bi.EndInit();
+            }
+            else
+            {
+
+            }
         }
     }
 }
---
+++ TheWeatherApp/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TheWeatherApp
+{
+    public class WeatherData
+    {
+        public DateTime Date { get; set; }
+
+        public string Forecast { get; set; }
+        //public Image Image { get; set; }
+    }
+}
