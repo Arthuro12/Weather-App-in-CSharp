ab6b5a608d793dec197d9896da0f1f89ad5429a4
arthur <arthurfoadjo@yahoo.com>
2022-06-24 16:48:05 +0200

    GUI

---
+++ TheWeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheWeatherApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Changes the image by getting the 
+            // Create source
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(@"/rain.jpg", UriKind.Relative);
+            bi.EndInit();
+            //Set the image source
+            headerImage.Source = bi;
+            //headerImage.Source = new BitmapImage(new Uri(@"/rain.jpg", UriKind.Relative));
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void buttonInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
