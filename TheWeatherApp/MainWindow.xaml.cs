eaee96cad66c34fec16af1fe9380a7d18a574ca5
arthur <arthurfoadjo@yahoo.com>
2022-06-25 22:57:52 +0200

    API call and response

--- TheWeatherApp/MainWindow.xaml.cs
+++ TheWeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,23 +24,61 @@ namespace TheWeatherApp
     {
         public WeatherData weather = new WeatherData();
 
+        public string cityName = "Paris";
+
+        public string resultJsonString;
+        //public string uri = String.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", cityName, apiKey);
+
         public MainWindow()
         {
             InitializeComponent();
 
             SetHeaderImage();
             SetDay(); //Date
+            GetApiResponse();
+            SetUiInfo();
          
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            
+            cityName = cityNameText.Text;
+            cityTitle.Content = cityName;
         }
 
+        /// <summary>
+        /// Display the inos of the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonInfo_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Hergestellt von Arthur Foadjo", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
 
+        /// <summary>
+        /// Get the response of our api call
+        /// </summary>
+        public void GetApiResponse()
+        {
+            string apiKey = "1a0ee8bf4a164f9a0a73f812da9853f2";
+            string baseApiString = String.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", cityName, apiKey);
+
+            using (WebClient wc = new WebClient())
+            {
+                // Loading of the JSON from the API
+                resultJsonString = wc.DownloadString(baseApiString);
+            }
+
+        }
+
+        /// <summary>
+        /// Parse the data to inject them in the user interface
+        /// </summary>
+        public void SetUiInfo()
+        {
+            JObject jo = JObject.Parse(resultJsonString);
+            MessageBox.Show(resultJsonString);
         }
 
         /// <summary>
@@ -64,6 +104,9 @@ namespace TheWeatherApp
             forecast3.Content = weather.Forecast;
         }
 
+        /// <summary>
+        /// Load the image of the Header
+        /// </summary>
         public void SetHeaderImage()
         {
             string meteo = "pluie";
